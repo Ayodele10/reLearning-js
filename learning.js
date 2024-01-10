47b849b2c57b8945881d42b221222ccb79eed952
Ayodele10 <egbayeloayodele72@gmail.com>
2024-01-10 22:18:02 +0100

    basis of functions and objects

---
+++ learning.js
@@ -0,0 +1,43 @@
+// using function parameters
+
+// Function is called, the return value will end up in x
+/*let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
+console.log(x)
+
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+  
+let value = toCelsius(12523)
+console.log(`the answer is ${value}`);
+*/
+
+
+//OBJECTS
+/*
+const Obj = {
+    name: 'Ayodele Funke Tayo',
+    class: '100lvl',
+    age: '18',
+    school: 'FUTA',
+    disp: function () {
+        console.log();
+    }
+}
+Obj.disp()
+
+  
+// hasOwnProperty code in js
+const object1 = new Object();
+object1.property1 = 42;
+
+console.log(object1.hasOwnProperty('property1')); // true
+*/
+
+
